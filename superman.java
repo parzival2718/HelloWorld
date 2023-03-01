f4c1b2a679fad71a117599b36473d09ab4274c07
Ram <ram@Rams-MacBook-Air.local>
2023-02-28 18:46:03 -0600

    New Project

---
+++ superman.java
@@ -0,0 +1,23 @@
+class batman
+{
+public static void main(String[] args)
+{
+int b=0;
+while(b<=21)
+{
+	if(b==0)
+	{
+		System.out.println(b + " is even");
+	}
+	else if(b%2==0)
+	{
+		System.out.println(b + " is Even");
+	}
+	else {
+		System.out.println(b + " is ODD");
+	}
+	b++;
+	
+}
+}
+}
